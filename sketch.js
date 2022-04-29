0df6df0be5c204ea6776a9ac417282f195b83df9
brigide <brigidegd@gmail.com>
2022-04-29 00:45:55 -0300

    added alpha to the rays simulating shadow

--- sketch.js
+++ sketch.js
@@ -3,13 +3,13 @@ let particle;
 
 function setup() {
     createCanvas(400, 400);
-    // for (let i = 0; i < 5; i++) {
-    //     let x1 = random(width);
-    //     let x2 = random(width);
-    //     let y1 = random(height);
-    //     let y2 = random(height);
-    //     walls.push(new Boundary(x1, x2, y1, y2));
-    // }
+    for (let i = 0; i < 5; i++) {
+        let x1 = random(width);
+        let x2 = random(width);
+        let y1 = random(height);
+        let y2 = random(height);
+        walls.push(new Boundary(x1, x2, y1, y2));
+    }
 
     //screen limit
     walls.push(new Boundary(0, 0, width, 0));
@@ -18,11 +18,11 @@ function setup() {
     walls.push(new Boundary(0, height, 0, 0));
 
     //house
-    walls.push(new Boundary(75, 75, width - 75, 75));
-    walls.push(new Boundary(width - 75, 75, width - 75, height - 75));
-    walls.push(new Boundary(width - 75, height - 75, width - width / 2 + 10, height - 75));
-    walls.push(new Boundary(width / 2 - 10, height - 75, 75, height - 75));
-    walls.push(new Boundary(75, height - 75, 75, 75));
+    // walls.push(new Boundary(75, 75, width - 75, 75));
+    // walls.push(new Boundary(width - 75, 75, width - 75, height - 75));
+    // walls.push(new Boundary(width - 75, height - 75, width - width / 2 + 10, height - 75));
+    // walls.push(new Boundary(width / 2 - 10, height - 75, 75, height - 75));
+    // walls.push(new Boundary(75, height - 75, 75, 75));
 
     particle = new Particle();
 }
